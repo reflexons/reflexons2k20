915a860f19c7ffe5c0f76c1a6ae612c87a469a79
reflexons2k18 <36360651+reflexons@users.noreply.github.com>
2020-03-03 10:55:41 +0530

    browser cache fix 1

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function( $ ) {
 
+  
   // Back to top button
   $(window).scroll(function() {
     if ($(this).scrollTop() > 100) {
@@ -145,3 +146,33 @@ jQuery(document).ready(function( $ ) {
 // custom code
 
 });
+(function (){
+  var rep = /.*\?.*/,
+      links = document.getElementsByTagName('link'),
+      scripts = document.getElementsByTagName('script'),
+      process_scripts = false;
+  for (var i=0;i<links.length;i++){
+    var link = links[i],
+        href = link.href;
+    if(rep.test(href)){
+      link.href = href+'&'+Date.now();
+    }
+    else{
+      link.href = href+'?'+Date.now();
+    }
+
+  }
+  if(process_scripts){
+    for (var i=0;i<scripts.length;i++){
+      var script = scripts[i],
+          src = script.src;
+      if(rep.test(src)){
+        script.src = src+'&'+Date.now();
+      }
+      else{
+        script.src = src+'?'+Date.now();
+      }
+
+    }
+  }
+})();
